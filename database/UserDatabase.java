4a6a2f3ea9fbe3110b11304a2e0b38f93998ffda
Arun Gowda <arungowdaarungowda36@gmail.com>
2024-10-27 12:45:57 +0530

    adding UserDatabase file into git from database branch. It contain data about users.

---
+++ database/UserDatabase.java
@@ -0,0 +1,13 @@
+package address_book_system.database;
+
+import address_book_system.entity.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDatabase {
+
+    public List<User> userData(){
+        return new ArrayList<User>();
+    }
+}
